8cb65d7506f487d5938156801f2d752d8ef432dc
susmitarajavarapu1221-cloud <susmitarajavarapu1221@gmail.com>
2026-05-20 16:32:47 +0530

    02_insert.sql

---
+++ 02_insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO departments VALUES
+(1,'Science','Vijayawada','Kumar','9876543210'),
+(2,'Maths','Guntur','Ramesh','9123456780'),
+(3,'English','Hyderabad','Suresh','9988776655');
+
+INSERT INTO teachers VALUES
+(1,'Ravi','Maths',50000),
+(2,'Sita','Science',55000),
+(3,'Anil','English',48000);
+
+INSERT INTO teacher_department VALUES
+(1,1),
+(2,2),
+(3,3);
